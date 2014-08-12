b60cc1f7ace4a53d16244acfcbc1e6a50064fe42
Bas Rustenburg <bas.rustenburg@choderalab.org>
2014-08-12 18:36:05 -0400

    Updated binary_mixture script.

--- scripts/binary_mixture_heats.py
+++ scripts/binary_mixture_heats.py
@@ -6,57 +6,42 @@ Script for generation of binary mixture ITC titrations.
 from simtk.unit import *
 
 from itctools.protocols import ITCProtocol, ITCExperimentSet, ITCExperiment, ITCHeuristicExperiment
-from itctools.chemicals import Solvent, Compound, SimpleSolution, PipettingLocation
-from itctools.labware import Labware
+from itctools.chemicals import Solvent, SimpleSolution
+from itctools.labware import Labware, PipettingLocation
  
 
-# Define solvents.
+
 #TODO command line specification of density and name
-water = Solvent('water', density=0.9970479*grams/milliliter)
-buffer = Solvent('buffer', density=1.014*grams/milliliter)
+# Mimic command line input by manually setting variables for now
+label1 = 'Water'
+dens1 = 0.9970479 *grams/milliliter
 
-# Define compounds.
+label2 = 'Dimethyl sulfoxide'
+dens2 = 1.092*grams/milliliter
 
-nguests = 6  # overnight from 5pm till 9am
-#nguests = 14 # number of guest compounds
-#nguests = 7 # number of guest compounds # DEBUG (one source plate only)
-host = Compound('host', molecular_weight=1162.9632*daltons, purity=0.7133)
-guest_molecular_weights = [209.12, 123.62, 153.65, 189.13, 187.11, 151.63, 135.64, 149.66, 163.69, 238.59, 147.65, 189.73, 173.68, 203.71]
-guest_compound_Ka = Quantity([21024287.6408, 13556262.0311, 81495.6444611, 1788684.70709, 2153596.60855, 769185.744612, 29967627.9188, 594389946.514, 2372114592.34, 683472.220385, 164811515.64, 6869559660.36, 28356538311.4, 396415131.021], liters/mole) #see dGtoKa.py
-guests = [ Compound(name='guest%02d' % (guest_index+1), molecular_weight=guest_molecular_weights[guest_index]*daltons, purity=0.975) for guest_index in range(nguests) ]
+# Define mixture components
+comp1 = Solvent(label1, density=dens1)
+comp2 = Solvent(label2, density=dens2)
+
+# Not using troughs. #
+#Definition of an example trough
+#comp1_trough = Labware(RackLabel=label1, RackType='Trough 100ml')
 
 
-# Define troughs on the instrument.
-
-water_trough = Labware(RackLabel='Water', RackType='Trough 100ml')
-buffer_trough = Labware(RackLabel='Buffer', RackType='Trough 100ml')
-
-# Define source labware.
-#source_plate = Labware(RackLabel='SourcePlate', RackType='12WellVialHolder')
+# Define source labware
 source_plate = Labware(RackLabel='SourcePlate', RackType='5x3 Vial Holder')
 
 # Define source solutions on the deck.one
-# TODO : Use actual compound and solvent masses.
-# NOTE: Host solution is diluted by 10x.
 
 host_solution = SimpleSolution(compound=host, compound_mass=16.76*milligrams, solvent=buffer, solvent_mass=10.2628*grams, location=PipettingLocation(source_plate.RackLabel, source_plate.RackType, 1))
 guest_solutions = list()
 
-#guest_compound_masses = Quantity([2.145, 1.268, 1.576, 1.940, 1.919, 1.555, 1.391, 1.535, 1.679, 2.447, 1.514, 1.946, 1.781, 2.089], milligrams)
-guest_compound_masses = Quantity([2.190, 2.115, 1.595, 1.930, 2.160, 1.580, 1.610, 1.660, 1.520, 2.750, 2.07, 1.98, 1.80, 2.22], milligrams)
-guest_solvent_masses = Quantity([10.2082, 16.7849, 10.1190, 9.9465, 11.2541, 10.1593, 11.5725, 10.8128, 9.0517, 11.2354, 13.6704, 10.1732, 10.1047, 10.6252], grams)
+# Define ITC protocolss.
 
-
-for guest_index in range(nguests):
-    guest_solutions.append( SimpleSolution(compound=guests[guest_index], compound_mass=guest_compound_masses[guest_index], solvent=buffer, solvent_mass=guest_solvent_masses[guest_index], location=PipettingLocation(source_plate.RackLabel, source_plate.RackType, 2+guest_index)) )
-
-# Define ITC protocol.
-
-# Protocol for 'control' titrations (water-water, buffer-buffer, titrations into buffer, etc.)
+# Protocol for 'control' titrations (water-water)
 control_protocol = ITCProtocol('control protocol', sample_prep_method='Plates Quick.setup', itc_method='ChoderaWaterWater.inj', analysis_method='Control')
 # Protocol for 1:1 binding analyis
-blank_protocol = ITCProtocol('1:1 binding protocol', sample_prep_method='Chodera Load Cell Without Cleaning Cell After.setup', itc_method='ChoderaHostGuest.inj', analysis_method='Onesite')
-binding_protocol = ITCProtocol('1:1 binding protocol', sample_prep_method='Plates Quick.setup', itc_method='ChoderaHostGuest.inj', analysis_method='Onesite')
+mixing_protocol = ITCProtocol('mixture protocol',  sample_prep_method='Plates Quick.setup', itc_method='ChoderaHostGuest.inj', analysis_method='Onesite')
 # Protocol for cleaning protocol
 cleaning_protocol = ITCProtocol('cleaning protocol', sample_prep_method='Plates Clean.setup', itc_method='water5inj.inj', analysis_method='Control')
 

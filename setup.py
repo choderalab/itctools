c22bb4c4cf4c1c1452920ffa67f36406982a7163
Bas Rustenburg <bas.rustenburg@choderalab.org>
2014-10-29 01:01:07 -0400

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='itctools',
+    version='0.0.1',
+    packages=['itctools'],
+    url='https://github.com/choderalab/itctools',
+    license='',
+    author='Bas Rustenburg, John Chodera',
+    author_email='bas.rustenburg@choderalab.org',
+    description='Tools for setting up ITC experiments in an automated fashion using the Tecan EVO and Auto-iTC 200',
+    test_suite='tests',
+    classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    # Indicate who your project is intended for
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering :: Chemistry',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    ],
+    extras_require = {
+    'test': ['coverage'],
+    },
+)

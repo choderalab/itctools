b21321f7b6101293888f413cc69aaf59fee0decd
Bas Rustenburg <bas.rustenburg@choderalab.org>
2014-10-31 18:17:56 -0400

    Added Compound nosetests.

--- tests/test_materials.py
+++ tests/test_materials.py
@@ -6,9 +6,11 @@ class TestMaterials(unittest.TestCase):
     """Validation of the materials submodule"""
 
     def _check_solvent(self, args):
+        """Validate the structure of a single or two argument Solvent."""
+        defaults = ['', None]
         water = materials.Solvent(*args)
-        if len(args) < 2:
-            args.append(None)
+         # Pad the list with elements from defaults till it is the same length
+        args.extend(defaults[len(args):])
         self.assertListEqual([water.name, water.density], args)
 
     def test_solvent(self):
@@ -16,3 +18,21 @@ class TestMaterials(unittest.TestCase):
         arguments = [['water'], ['water', 0.9970479 * unit.grams / unit.milliliter]]
         for args in arguments:
             self._check_solvent(args)
+
+    def _check_compound(self, args):
+        """Validate structure of single, two or three arguments"""
+        defaults = ['', None, 1.00]
+        compound = materials.Compound(*args)
+        # Pad the list with elements fom defaults till it is the same length
+        args.extend(defaults[len(args):])
+        self.assertListEqual([compound.name,compound.molecular_weight,compound.purity, ], args)
+
+    def test_compound(self):
+        """Ensure Compound has correct parameter assignment"""
+        arguments = [['nacl'],
+                     ['imatinib mesylate', 589.7 * unit.grams/unit.mole],
+                     ['compound1', 209.12 * unit.grams/unit.mole, 0.975]
+        ]
+
+        for args in arguments:
+            self._check_compound(args)

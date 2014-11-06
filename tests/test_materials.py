0956034167ae48f6cd34bda29ca2d185403f9f41
Bas Rustenburg <bas.rustenburg@choderalab.org>
2014-11-05 23:47:45 -0500

    Stricter tests. Bug in SimpleMixtures found.

--- tests/test_materials.py
+++ tests/test_materials.py
@@ -114,12 +114,7 @@ class TestMaterials(unittest.TestCase):
             'solvent_mass': solvent_mass,
             'concentration': (compound_mass / mock_compound.molecular_weight * mock_compound.purity) / (solvent_mass / mock_solvent.density),
         }
-        try:  # python 3.2 +
-            self.assertCountEqual(
-                expected_structure, simple_solution.__dict__)
-        except AttributeError:
-            self.assertItemsEqual(
-                expected_structure, simple_solution.__dict__)
+        self.assertDictEqual(expected_structure, simple_solution.__dict__)
 
     def test_simple_solution_solvent_compatibility(self):
         """Ensure that SimpleSolution is compatible with Solvent"""
@@ -155,13 +150,7 @@ class TestMaterials(unittest.TestCase):
             'solvent_mass': solvent_mass,
             'concentration': (compound_mass / mock_compound.molecular_weight * mock_compound.purity) / (solvent_mass / solvent.density),
         }
-
-        try:  # python 3.2 +
-            self.assertCountEqual(
-                expected_structure, simple_solution.__dict__)
-        except AttributeError:
-            self.assertItemsEqual(
-                expected_structure, simple_solution.__dict__)
+        self.assertDictEqual(expected_structure, simple_solution.__dict__)
 
     def test_simple_solution_compound_compatibility(self):
         """Ensure that SimpleSolution is compatible with Compound"""
@@ -198,9 +187,65 @@ class TestMaterials(unittest.TestCase):
             'concentration': (compound_mass / compound.molecular_weight * compound.purity) / (solvent_mass / mock_solvent.density),
         }
 
-        try:  # python 3.2+
-            self.assertCountEqual(
-                expected_structure, simple_solution.__dict__)
-        except AttributeError:
-            self.assertItemsEqual(
-                expected_structure, simple_solution.__dict__)
+        self.assertDictEqual(expected_structure, simple_solution.__dict__)
+
+    def test_simple_mixture(self):
+        """Ensure that SimpleMixture has correct parameter assignment"""
+        mock_liquid1 = Mock(
+            name='water',
+            density=0.9970479 *
+            unit.grams /
+            unit.milliliter,
+            molecular_weight=18.01528 *
+            unit.gram /
+            unit.mole)
+        mock_liquid2 = Mock(
+            name='ethanol',
+            density=0.789 *
+            unit.grams /
+            unit.milliliter,
+            molecular_weight=46.07 *
+            unit.gram /
+            unit.mole)
+        liquids = [mock_liquid1, mock_liquid2]
+        mock_location1 = Mock(
+            RackLabel='SourcePlate',
+            RackType='5x3 Vial Holder',
+            Position=1)
+        mock_location2 = Mock(
+            RackLabel='SourcePlate',
+            RackType='5x3 Vial Holder',
+            Position=2)
+        locations = [mock_location1, mock_location2]
+        fractions = [0.6, 0.4]
+        molar_volumes = [liq.molecular_weight / liq.density for liq in liquids]
+
+        normalizing_mass = 0 * unit.grams / unit.mole
+        for i, liq in enumerate(liquids):
+            normalizing_mass += liq.molecular_weight * fractions[i]
+
+        mass_fractions = [
+            fractions[i] *
+            liq.molecular_weight /
+            normalizing_mass for i,
+            liq in enumerate(liquids)]
+
+        normalizing_volume = 0 * unit.liter / unit.mole
+        for i, volume in enumerate(molar_volumes):
+            normalizing_volume += fractions[i] * volume
+
+        volume_fractions = [
+            fractions[i] *
+            volume /
+            normalizing_volume for i,
+            volume in enumerate(molar_volumes)]
+        simple_mixture = materials.SimpleMixture(liquids, fractions, locations)
+
+        expected_structure = {
+            'components': [mock_liquid1, mock_liquid2],
+            'locations': [mock_location1, mock_location2],
+            'massfractions': mass_fractions,
+            'molefractions': fractions,
+            'volumefractions': volume_fractions,
+        }
+        self.assertDictEqual(expected_structure, simple_mixture.__dict__)
